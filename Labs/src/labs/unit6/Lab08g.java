f948fbd2eadd68cd653dc36168b245e1c0f058c7
Soccervolley5 <BlackC0650@sduhsd.lan>
2018-02-08 09:09:33 -0800

    Completed unit 6 {Almost}
    need to finish i-h

---
+++ Labs/src/labs/unit6/Lab08g.java
@@ -0,0 +1,22 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+package labs.unit6;
+import static java.lang.System.*;
+
+public class Lab08g
+{
+	public static void main ( String[] args )
+	{
+		LoopStats loopStats = new LoopStats();
+		loopStats.setNums(1, 5);
+		System.out.println(loopStats);
+		loopStats.setNums(2, 8);
+		System.out.println(loopStats);
+		loopStats.setNums(5, 15);
+		System.out.println(loopStats);
+					
+	}
+}
