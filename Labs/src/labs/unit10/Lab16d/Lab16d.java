47bd7eaa026ca5d521f5894cf24a4c7dbff4e64d
Soccervolley5 <BlackC0650@sduhsd.lan>
2018-03-08 09:25:41 -0800

    Finish Lab10
    Finish the first one

---
+++ Labs/src/labs/unit10/Lab16d/Lab16d.java
@@ -0,0 +1,17 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+package labs.unit10.Lab16d;
+import static java.lang.System.*;
+
+public class Lab16d
+{
+	public static void main( String args[] )
+	{
+		//make a new MadLib
+		
+		out.println("\n");
+	}
+}
