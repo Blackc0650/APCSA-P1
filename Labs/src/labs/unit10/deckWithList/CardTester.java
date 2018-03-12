177156b0a41a17abb3df3536570aef9779d68bf8
Soccervolley5 <BlackC0650@sduhsd.lan>
2018-03-12 08:19:00 -0700

    Added Deck With List to github
    + Uploaded the Deck class using a List

---
+++ Labs/src/labs/unit10/deckWithList/CardTester.java
@@ -0,0 +1,19 @@
+package labs.unit10.deckWithList;
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card a = new Card("?","hearts",4);
+		Card b = new Card("?","hearts",4);
+		Card c = new Card("?","clubs",5);
+		System.out.println(a.matches(b));
+		System.out.println(c.pointValue() + " : " + a.rank() + " : " + b.suit());
+	}
+}
