1f7c420245459e3b19b871ad9115f019fa0d6fbc
Soccervolley5 <BlackC0650@sduhsd.lan>
2018-03-29 09:25:04 -0700

    + Almost done with Lab 13
    + Need to finish a couple things

---
+++ Labs/src/labs/unit13/QuickSort.java
@@ -0,0 +1,49 @@
+package labs.unit13;
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  - 
+
+import static java.lang.System.*;
+import java.util.Arrays;		//use Arrays.toString() to help print out the array
+
+public class QuickSort
+{
+	private static int passCount;
+
+	public static void quickSort(Comparable[] list)
+	{
+
+
+
+
+	}
+
+
+	private static void quickSort(Comparable[] list, int low, int high)
+	{
+
+
+
+
+
+
+
+	}
+
+
+	private static int partition(Comparable[] list, int low, int high)
+	{
+		int pivot = (Integer) list[0];
+		int bot = low - 1;
+		int top = high + 1;
+		while(bot < top) {
+			if(bot + 1 < pivot && top - 1 > pivot) {
+				
+			}
+		}
+		
+		return 0;
+	}
+}
