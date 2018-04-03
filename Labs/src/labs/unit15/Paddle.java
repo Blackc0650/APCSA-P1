80b1f0d618ffdc4a60ee999e5b9689b0fac55603
Blackc0650 <kylejeffreywhite05@gmail.com>
2018-04-03 09:44:54 -0700

    Added Unit 15
    Unit 15 Added

---
+++ Labs/src/labs/unit15/Paddle.java
@@ -0,0 +1,53 @@
+package labs.unit15;
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Paddle extends Block {
+    //instance variables
+    private int speed;
+    public Paddle() {
+        super(10, 10);
+        speed = 5;
+    }
+    //add the other Paddle constructors
+    public Paddle(int x, int y) {
+    	super(x, y);
+    }
+    public Paddle(int x, int y, int width) {
+    	super(x, y);
+    }
+    public Paddle(int x, int y, int width, int height, int speed) {
+    	super(x, y, width, height);
+    	this.speed = speed;
+    	
+    }
+    public Paddle(int x, int y, int width, int height, Color color, int speed) {
+    	super(x, y, width, height, color);
+    	this.speed = speed;
+    }
+    public void moveUpAndDraw(Graphics window) {
+    	Color old = getColor();
+    	setColor(Color.WHITE);
+    	draw(window);
+    	setY(getY() - speed);
+    	setColor(old);
+    	draw(window);
+    	//setHeight(getHeight() - speed);
+    }
+    public void moveDownAndDraw(Graphics window) {
+    	Color old = getColor();
+    	setColor(Color.WHITE);
+    	draw(window);
+    	setY(getY() + speed);
+    	setColor(old);
+    	draw(window);
+    }
+    //add get methods
+    //add a toString() method
+}
